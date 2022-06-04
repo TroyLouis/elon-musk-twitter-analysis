9c0c575f267887f1d1193f222ec070917bf5fc4e
Troy Arnold <troylarnold@gmail.com>
2022-06-03 18:22:15 -0700

    Working twitter api call

--- twitter/twitter_scraper.py
+++ twitter/twitter_scraper.py
@@ -1,1 +1,35 @@
-import tweepy
+import json, requests
+
+bearer_token='AAAAAAAAAAAAAAAAAAAAAG5FdQEAAAAA%2BVRNt4DUG%2FIQprnNjt6js7F9Q3M%3D1xB0Tzo8GaSusoE8yImGKYu3r1PQGZZmYcHyQLrODViNkkFvcd'
+
+search_url = "https://api.twitter.com/2/tweets/search/recent"
+
+# Optional params: start_time,end_time,since_id,until_id,max_results,next_token,
+# expansions,tweet.fields,media.fields,poll.fields,place.fields,user.fields
+query_params = {'query': '(from:elonmusk -is:retweet)','tweet.fields': 'author_id'}
+
+
+def bearer_oauth(r):
+    """
+    Method required by bearer token authentication.
+    """
+
+    r.headers["Authorization"] = f"Bearer {bearer_token}"
+    r.headers["User-Agent"] = "v2RecentSearchPython"
+    return r
+
+def connect_to_endpoint(url, params):
+    response = requests.get(url, auth=bearer_oauth, params=params)
+    print(response.status_code)
+    if response.status_code != 200:
+        raise Exception(response.status_code, response.text)
+    return response.json()
+
+
+def main():
+    json_response = connect_to_endpoint(search_url, query_params)
+    print(json.dumps(json_response, indent=4, sort_keys=True))
+
+
+if __name__ == "__main__":
+    main()
