79ad250ab97de8f6e9ca8e0edf889a5108405792
Troy Arnold <troylarnold@gmail.com>
2022-06-04 15:59:59 -0700

    Attempting to add pagination

--- twitter/scraper.py
+++ twitter/scraper.py
@@ -1,36 +1,14 @@
 import tweepy
 import os
-import json
-import requests
 
 bearer_token = os.environ.get('BEARER')
 
-search_url = "https://api.twitter.com/2/tweets/search/recent"
+client = tweepy.Client(bearer_token)
 
-# Optional params: start_time,end_time,since_id,until_id,max_results,next_token,
-# expansions,tweet.fields,media.fields,poll.fields,place.fields,user.fields
-query_params = {'query': '(from:elonmusk -is:retweet)','tweet.fields': 'author_id'}
-
-def bearer_oauth(r):
-    """
-    Method required by bearer token authentication.
-    """
-
-    r.headers["Authorization"] = f"Bearer {bearer_token}"
-    r.headers["User-Agent"] = "v2RecentSearchPython"
-    return r
-
-def connect_to_endpoint(url, params):
-    response = requests.get(url, auth=bearer_oauth, params=params)
-    print(response.status_code)
-    if response.status_code != 200:
-        raise Exception(response.status_code, response.text)
-    return response.json()
-
+tweets = client.get_users_tweets(id="44196397")
 
 def main():
-    json_response = connect_to_endpoint(search_url, query_params)
-    print(json.dumps(json_response, indent=4, sort_keys=True))
+    print(tweets.meta['next_token'])
 
 
 if __name__ == "__main__":
