ac43f0fbb9861f77e8e583a816b322431e08f57d
Troy Arnold <troylarnold@gmail.com>
2022-06-06 18:23:24 -0700

    Added paginator, potential issue with it running Python 3.8

--- twitter/scraper.py
+++ twitter/scraper.py
@@ -11,62 +11,28 @@ access_secret = os.environ.get('Twitter_Access_Secret')
 bearer = os.environ.get('bearer_token')
 
 def create_client():
-    """
-
-    :return: Twitter API authentication client
-    """
     client = tweepy.Client( bearer_token=bearer,
                         return_type=requests.Response,
                         wait_on_rate_limit=True)
     return client
 
-def create_query(username):
-    """
+def create_paginator(authenticated_client):
 
-    :param username: Twitter handle
-    :return: Twitter API query string
-    """
-    query = f'from:{username}'
-    return query
+    paginator = tweepy.Paginator(
+        authenticated_client.search_recent_tweets,
+        query='from:elonmusk',
+        tweet_fields=['author_id', 'id', 'created_at'],
+        max_results=100,
+        limit=5)
 
-def call_api(client, query):
-    """
-
-    :param client: Twitter API authentication client
-    :param query: Twitter query string
-    :return: API call data
-    """
-    tweets = client.search_recent_tweets(query=query,
-                                    tweet_fields=['author_id', 'id', 'created_at','source'],
-                                     max_results=1000)
-    return tweets
-
-def data_to_df(data):
-    """
-
-    :param data: API call data
-    :return: Pandas dataframe of data
-    """
-    all_tweets = data.json()
-    tweets_data = all_tweets['data']
-    df = pd.json_normalize(tweets_data)
-    return df
-
-def df_to_csv(df):
-    """
-
-    :param df: Pandas dataframe
-    :return: CSV file of dataframe
-    """
-    df.to_csv('data.csv', encoding='utf-8', index=False)
+    return paginator
 
 def main():
     client = create_client()
-    query = create_query('elonmusk')
-    data = call_api(client,query)
-    df = data_to_df(data)
-    print(df)
-    df_to_csv(df)
+
+    for tweet in tweepy.Paginator(client.search_recent_tweets, "Tweepy",
+                                  max_results=100).flatten(limit=250):
+        print(tweet.id)
 
 if __name__ == "__main__":
     main()
