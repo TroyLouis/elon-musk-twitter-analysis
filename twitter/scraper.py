6dff2197e275e7c694b56d0a7c56a9bf073f982a
Troy Arnold <troylarnold@gmail.com>
2022-06-06 17:09:37 -0700

    Working API Call

--- twitter/scraper.py
+++ twitter/scraper.py
@@ -1,15 +1,31 @@
 import tweepy
+import requests
 import os
+import pandas as pd
 
-bearer_token = os.environ.get('BEARER')
+#tokens
+api_key = os.environ.get('Twitter_API_Key')
+api_secret = os.environ.get('Twitter_API_Secret')
+access_token = os.environ.get('Twitter_Access_Token')
+access_secret = os.environ.get('Twitter_Access_Secret')
+bearer = os.environ.get('bearer_token')
 
-client = tweepy.Client(bearer_token)
+client = tweepy.Client( bearer_token=bearer,
+                        return_type=requests.Response,
+                        wait_on_rate_limit=True)
 
-tweets = client.get_users_tweets(id="44196397")
+query = 'from:elonmusk -is:retweet'
+tweets = client.search_recent_tweets(query=query,
+                                    tweet_fields=['author_id', 'created_at'],
+                                     max_results=20)
+
+all_tweets = tweets.json()
+tweets_data = all_tweets['data']
+df = pd.json_normalize(tweets_data)
+print(df.head)
 
 def main():
-    print(tweets.meta['next_token'])
-
+    pass
 
 if __name__ == "__main__":
     main()
