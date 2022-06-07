5da5562ab9d67acc297ae32d3a3f373864939236
Troy Arnold <troylarnold@gmail.com>
2022-06-06 17:37:21 -0700

    Created functions to scrape data

--- twitter/scraper.py
+++ twitter/scraper.py
@@ -3,29 +3,70 @@ import requests
 import os
 import pandas as pd
 
-#tokens
+# global tokens
 api_key = os.environ.get('Twitter_API_Key')
 api_secret = os.environ.get('Twitter_API_Secret')
 access_token = os.environ.get('Twitter_Access_Token')
 access_secret = os.environ.get('Twitter_Access_Secret')
 bearer = os.environ.get('bearer_token')
 
-client = tweepy.Client( bearer_token=bearer,
+def create_client():
+    """
+
+    :return: Twitter API authentication client
+    """
+    client = tweepy.Client( bearer_token=bearer,
                         return_type=requests.Response,
                         wait_on_rate_limit=True)
+    return client
 
-query = 'from:elonmusk -is:retweet'
-tweets = client.search_recent_tweets(query=query,
-                                    tweet_fields=['author_id', 'created_at'],
-                                     max_results=20)
+def create_query(username):
+    """
 
-all_tweets = tweets.json()
-tweets_data = all_tweets['data']
-df = pd.json_normalize(tweets_data)
-print(df.head)
+    :param username: Twitter handle
+    :return: Twitter API query string
+    """
+    query = f'from:{username}'
+    return query
+
+def call_api(client, query):
+    """
+
+    :param client: Twitter API authentication client
+    :param query: Twitter query string
+    :return: API call data
+    """
+    tweets = client.search_recent_tweets(query=query,
+                                    tweet_fields=['author_id', 'id', 'created_at','source'],
+                                     max_results=1000)
+    return tweets
+
+def data_to_df(data):
+    """
+
+    :param data: API call data
+    :return: Pandas dataframe of data
+    """
+    all_tweets = data.json()
+    tweets_data = all_tweets['data']
+    df = pd.json_normalize(tweets_data)
+    return df
+
+def df_to_csv(df):
+    """
+
+    :param df: Pandas dataframe
+    :return: CSV file of dataframe
+    """
+    df.to_csv('data.csv', encoding='utf-8', index=False)
 
 def main():
-    pass
+    client = create_client()
+    query = create_query('elonmusk')
+    data = call_api(client,query)
+    df = data_to_df(data)
+    print(df)
+    df_to_csv(df)
 
 if __name__ == "__main__":
     main()
